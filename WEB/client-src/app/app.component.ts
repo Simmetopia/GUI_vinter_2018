a8b084a60970b05e0f25d0204366eda876e88582
Simon Egeberg <201406253@post.au.dk>
2018-01-15 19:59:54 +0100

    Drag and drop

--- WEB/client-src/app/app.component.ts
+++ WEB/client-src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { DragulaService } from 'ng2-dragula';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  simpleDrop: any = null;
+  simpleDrop1(ev: any) {
+    console.log('dropped in material');
+  }
 }
--- WEB/client-src/app/app.module.ts
+++ WEB/client-src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { DndModule } from 'ng2-dnd';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, MaterialModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    DndModule.forRoot()
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
