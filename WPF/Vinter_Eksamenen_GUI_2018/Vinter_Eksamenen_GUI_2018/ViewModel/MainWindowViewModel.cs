89a115ddf0f3f7f7e1505a1c8b91a5f24986978c
Simon Egeberg <201406253@post.au.dk>
2018-01-15 18:26:16 +0100

    donger

---
+++ WPF/Vinter_Eksamenen_GUI_2018/Vinter_Eksamenen_GUI_2018/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Vinter_Eksamenen_GUI_2018.ViewModel
+{
+    public class MainWindowViewModel
+    {
+        
+    }
+}
---
+++ WPF/Vinter_Eksamenen_GUI_2018/Vinter_Eksamenen_GUI_2018/ViewModel/Window1ViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vinter_Eksamenen_GUI_2018.ViewModel
+{
+    public class Window1ViewModel
+    {
+    }
+}
